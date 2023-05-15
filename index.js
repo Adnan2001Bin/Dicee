812f8e5159939d1f0ac9c2c06aa9082c5b29a5c1
Adnan2001Bin <110903274+Adnan2001Bin@users.noreply.github.com>
2023-05-15 17:35:18 +0600

    Add Initial Dicee game files

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var randomNum1 = Math.floor(Math.random() *6) + 1;
+var randomDiceeImg = "Dicee" + randomNum1 + ".png";
+var randomImgsrc = "Image/" + randomDiceeImg ;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImgsrc);
+
+
+var randomNum2 = Math.floor(Math.random() *6) + 1;
+var randomDiceeImg2 = "Dicee" + randomNum2 + ".png";
+var randomImgsrc2 = "Image/" + randomDiceeImg2 ;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImgsrc2);
+
+if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerHTML= "Player 1 Wins! ";
+}
+else if (randomNum1 < randomNum2) {
+    document.querySelector("h1").innerHTML= "Player 2 Wins! ";
+} 
+else {
+    document.querySelector("h1").innerHTML= "Draw! ";
+}
