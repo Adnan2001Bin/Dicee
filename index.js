422cfd3249d3794c21fa391e4966fe13c147c0de
Adnan2001Bin <110903274+Adnan2001Bin@users.noreply.github.com>
2023-07-12 17:26:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+document.querySelector("button").addEventListener("click" , handclick);
 
-var randomNum1 = Math.floor(Math.random() *6) + 1;
-var randomDiceeImg = "Dicee" + randomNum1 + ".png";
-var randomImgsrc = "Image/" + randomDiceeImg ;
+function handclick() {
+    var buttonInnerHTML = this.innerHTML;
+    var randomNum1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceeImg = "Dicee" + randomNum1 + ".png";
+    var randomImgsrc = "Image/" + randomDiceeImg;
 
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImgsrc);
+    var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", randomImgsrc);
 
 
-var randomNum2 = Math.floor(Math.random() *6) + 1;
-var randomDiceeImg2 = "Dicee" + randomNum2 + ".png";
-var randomImgsrc2 = "Image/" + randomDiceeImg2 ;
+    var randomNum2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceeImg2 = "Dicee" + randomNum2 + ".png";
+    var randomImgsrc2 = "Image/" + randomDiceeImg2;
 
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImgsrc2);
+    var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImgsrc2);
 
-if (randomNum1 > randomNum2) {
-    document.querySelector("h1").innerHTML= "Player 1 Wins! ";
+    if (randomNum1 > randomNum2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins! ";
+    }
+    else if (randomNum1 < randomNum2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! ";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw! ";
+    }
+    buttonAnimation(buttonInnerHTML);
 }
-else if (randomNum1 < randomNum2) {
-    document.querySelector("h1").innerHTML= "Player 2 Wins! ";
-} 
-else {
-    document.querySelector("h1").innerHTML= "Draw! ";
+
+function buttonAnimation() {
+    var activeButton = document.querySelector("button");
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100);
 }
